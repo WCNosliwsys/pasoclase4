ef8a73c7cf68d5175ca8b90e53ef5126457160bc
Ing Wilson <nosliwsys@gmail.com>
2019-01-24 18:00:41 -0500

    Servicio

--- app/src/main/java/com/a952000243/ingwilson/nosliwsys/pasoclase4/Servicio.java
+++ app/src/main/java/com/a952000243/ingwilson/nosliwsys/pasoclase4/Servicio.java
@@ -1,13 +1,29 @@
 package com.a952000243.ingwilson.nosliwsys.pasoclase4;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Servicio extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class Servicio extends Activity {
+    @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_servicio);
+        Button arrancar = (Button) findViewById(R.id.boton_arrancar);
+        arrancar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startService(new Intent(Servicio.this,
+                        ServicioMusica.class));
+            }
+        });
+        Button detener = (Button) findViewById(R.id.boton_detener);
+        detener.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                stopService(new Intent(Servicio.this,
+                        ServicioMusica.class));
+            }
+        });
     }
 }
